b2aba76b4765f51742d178b12c9b6101466c7314
Phil Olalere <olalerephilip@gmail.com>
2022-02-07 05:19:38 +0100

    fixed bedcrome icon on mobileView

--- src/components/incs/Navbar.js
+++ src/components/incs/Navbar.js
@@ -5,7 +5,7 @@ import img from "../images/admin.png";
 
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 20) {
+        if (window.scrollY > 50) {
           document.getElementById('navbar_top').classList.add('fixed-top');
           // add padding top to show content behind navbar
           let navbar_height = document.querySelector('.navbar').offsetHeight;
@@ -20,13 +20,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function Navbar(){
     return(
-        <nav id="navbar_top" className="navbar navbar-expand-lg navbar-white bg-white tableFixHead">
+        <nav id="navbar_top" className="navbar navbar-expand-lg navbar-light bg-white">
             <div className="container-fluid">
                 
                 {/* <Link to = "/" class = "navbar-brand"> PhilsvibeTheMusician </Link> */}
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
+                    
                 </button>
 
                 <div className="collapse navbar-collapse" style = {{paddingTop:30}} id="navbarSupportedContent">
